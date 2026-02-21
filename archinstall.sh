9fc6e2d526fe96f504ad49f5931766d6d6162375
BaalskultXLIX <baalskultxlix1976@gmail.com>
2026-02-21 12:44:16 +0100

    Fix chroot command and unmount before reboot
    Added exit command after chroot operations and unmounting.

--- archinstall.sh
+++ archinstall.sh
@@ -28,5 +28,8 @@ pacstrap -K /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # I don't know how to change the terminal without aborting the archinstall script.
-arch-chroot /mnt bash -c " ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime;  hwclock --systohc;  echo "de_DE.UTF-8\ UTF-8" >> /etc/locale.gen; locale-gen; echo "de_DE.UTF-8" > /etc/locale.conf; echo "KEYMAP=de-latin1" >  /etc/vconsole.conf; echo "archlinux" >  /etc/hostname; pacman -Syyu networkmanager grub nano efibootmgr plasma plasma-login-manager  acpid avahi iwd fish --noconfirm; sytemctl enable acpid avahi-daemon NetworkManager iwd systemd-timesyncd fstrim.timer plasmalogin.service; mkinitcpio -P; passwd; useradd -m -g users -s /bin/fish ilja; passwd ilja; usermod -aG wheel,users ilja; grub-install --target=x86_64-efi --efi-directory=boot/ --bootloader-id=GRUB; grub-mkconfig -o /boot/grub/grub.cfg;"
+arch-chroot /mnt bash -c " ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime;  hwclock --systohc;  echo "de_DE.UTF-8\ UTF-8" >> /etc/locale.gen; locale-gen; echo "de_DE.UTF-8" > /etc/locale.conf; echo "KEYMAP=de-latin1" >  /etc/vconsole.conf; echo "archlinux" >  /etc/hostname; pacman -Syyu networkmanager grub nano efibootmgr plasma plasma-login-manager  acpid avahi iwd fish --noconfirm; systemctl enable acpid avahi-daemon NetworkManager iwd systemd-timesyncd fstrim.timer plasmalogin.service; mkinitcpio -P; passwd; useradd -m -g users -s /bin/fish ilja; passwd ilja; usermod -aG wheel,users ilja; grub-install --target=x86_64-efi --efi-directory=boot/ --bootloader-id=GRUB; grub-mkconfig -o /boot/grub/grub.cfg; exit"
 
+umount -R /mnt
+
+reboot 
