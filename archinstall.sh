f5ea85919e468dda6a95e285edfd8c47da341898
BaalskultXLIX <baalskultxlix1976@gmail.com>
2026-02-21 21:43:51 +0100

    Refactor archinstall.sh for better readability and updates

--- archinstall.sh
+++ archinstall.sh
@@ -3,18 +3,30 @@
 # WARNING This archinstall is for the archlinux live iso you cant damang your system
 # WARNING This archinstall is for my pc i have a full amd system.
 # WARNING This is my first archinstall.
+# WARNING When you use a old PC or slow internet please do not use my sript
 # You need to stetup your Wifi your self. I dont know how do dish on Iwctl withe a command. I know how to stetup Iwctl but i dont now your wifi and password :C.
 
-loadkeys de-latin1
-# You need to stetup your own Partitions i dont now how do that withe fdisk in a command please visit the archwiki for fdisk layouts.
+swapoff /dev/sda*
+
+umount -lRA /dev/sda*
+
+umount -RA /dev/sda*
+
 # I'm using GPT not MBR please visit the archwiki for MBR.
-fdisk /dev/sda
+
+parted /dev/sda --script mklabel gpt
+
+parted /dev/sda --script mkpart "EFI" fat32 1MiB 1025MiB set 1 esp on
+
+parted /dev/sda --script mkpart "swap" linux-swap 1025MiB 9217MiB
+
+parted /dev/sda --script mkpart "Linux" btrfs 9217MiB 100%
 #I'm using fat 32 for GRUB.
 mkfs.vfat -F 32 /dev/sda1
 # I'm using swap you can remove the command if you not useing swap.
-mkswap /dev/sda2
+mkswap -f /dev/sda2
 # I'm using btrfs you can rebles the mkfs.btrfs to mkfs.ext4.
-mkfs.btrfs  /dev/sda3
+mkfs.btrfs -f /dev/sda3
 
 mount /dev/sda3 /mnt
 
@@ -28,8 +40,38 @@ pacstrap -K /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # I don't know how to change the terminal without aborting the archinstall script.
-arch-chroot /mnt bash -c " ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime;  hwclock --systohc;  echo "de_DE.UTF-8\ UTF-8" >> /etc/locale.gen; locale-gen; echo "de_DE.UTF-8" > /etc/locale.conf; echo "KEYMAP=de-latin1" >  /etc/vconsole.conf; echo "archlinux" >  /etc/hostname; pacman -Syyu networkmanager grub nano efibootmgr plasma plasma-login-manager  acpid avahi iwd fish --noconfirm; systemctl enable acpid avahi-daemon NetworkManager iwd systemd-timesyncd fstrim.timer plasmalogin.service; mkinitcpio -P; passwd; useradd -m -g users -s /bin/fish user; passwd user; usermod -aG wheel,users user; grub-install --target=x86_64-efi --efi-directory=boot/ --bootloader-id=GRUB; grub-mkconfig -o /boot/grub/grub.cfg; exit"
+arch-chroot /mnt bash -c "
+
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime;
+
+hwclock --systohc;
+
+echo "de_DE.UTF-8\ UTF-8" >> /etc/locale.gen;
+
+locale-gen;
+
+echo "de_DE.UTF-8" > /etc/locale.conf;
+
+echo "KEYMAP=de-latin1" >  /etc/vconsole.conf;
+
+echo "archlinux" >  /etc/hostname;
+
+yes | pacman -Syyu networkmanager grub nano efibootmgr plasma plasma-login-manager  acpid avahi iwd fish --disable-download-timeout --noconfirm;
+
+systemctl enable acpid avahi-daemon NetworkManager iwd systemd-timesyncd fstrim.timer plasmalogin.service;
+
+mkinitcpio -P;
+
+passwd;
+
+useradd -m -g users -s /bin/fish user;
+
+passwd user; usermod -aG wheel,users user;
+
+grub-install --target=x86_64-efi --efi-directory=boot/ --bootloader-id=GRUB;
+
+grub-mkconfig -o /boot/grub/grub.cfg"
 
 umount -R /mnt
 
-reboot 
+reboot now
