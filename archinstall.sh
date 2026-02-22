866aae5b098a0ece1dcf0d6a2ebc63aae3ac1e3e
BaalskultXLIX <baalskultxlix1976@gmail.com>
2026-02-22 19:11:45 +0100

    Add license and comments to archinstall.sh
    Added license information and comments to the script.

--- archinstall.sh
+++ archinstall.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+# archinstall.sh - Automated Arch Linux installation script
+# Copyright (c) 2026 BaalskultXLIX
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+
 # WARNING Please read my archinstall kommins and sorry for my bad english.
 # WARNING I'm not a linux pro.
 # WARNING This archinstall is for the archlinux live iso you cant damang your system
