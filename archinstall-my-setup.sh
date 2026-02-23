162966449443954b9c495d8b3f7c0e98095a4f46
BaalskultXLIX <baalskultxlix1976@gmail.com>
2026-02-23 17:04:45 +0100

    Update pacman.conf and fish configuration files
    Add pacman configuration and fish shell setup.

--- archinstall-my-setup.sh
+++ archinstall-my-setup.sh
@@ -160,6 +160,100 @@ pacstrap -K /mnt base linux linux-firmware
 # Fstab generate an fstab file.
 genfstab -U /mnt >> /mnt/etc/fstab
 
+echo -e "
+#
+# /etc/pacman.conf
+#
+# See the pacman.conf(5) manpage for option and repository directives
+
+#
+# GENERAL OPTIONS
+#
+[options]
+# The following paths are commented out with their default values listed.
+# If you wish to use different paths, uncomment and update the paths.
+#RootDir     = /
+#DBPath      = /var/lib/pacman/
+CacheDir    = /var/cache/pacman/pkg/
+#LogFile     = /var/log/pacman.log
+#GPGDir      = /etc/pacman.d/gnupg/
+#HookDir     = /etc/pacman.d/hooks/
+HoldPkg     = pacman glibc
+#XferCommand = /usr/bin/curl -L -C - -f -o %o %u
+#XferCommand = /usr/bin/wget --passive-ftp -c -O %o %u
+#CleanMethod = KeepInstalled
+Architecture = auto
+
+# Pacman won't upgrade packages listed in IgnorePkg and members of IgnoreGroup
+#IgnorePkg   =
+#IgnoreGroup =
+
+#NoUpgrade   =
+#NoExtract   =
+
+# Misc options
+#UseSyslog
+ILoveCandy
+DisableDownloadTimeout
+Color
+#NoProgressBar
+CheckSpace
+#VerbosePkgLists
+ParallelDownloads = 10
+DownloadUser = alpm
+#DisableSandboxFilesystem
+#DisableSandboxSyscalls
+
+# By default, pacman accepts packages signed by keys that its local keyring
+# trusts (see pacman-key and its man page), as well as unsigned packages.
+SigLevel    = Required DatabaseOptional
+LocalFileSigLevel = Optional
+#RemoteFileSigLevel = Required
+
+
+#
+# Repository entries are of the format:
+#       [repo-name]
+#       Server = ServerName
+#       Include = IncludePath
+#
+# The header [repo-name] is crucial - it must be present and
+# uncommented to enable the repo.
+#
+
+# The testing repositories are disabled by default. To enable, uncomment the
+# repo name header and Include lines. You can add preferred servers immediately
+# after the header, and they will be used before the default mirrors.
+
+#[core-testing]
+#Include = /etc/pacman.d/mirrorlist
+
+[core]
+Include = /etc/pacman.d/mirrorlist
+
+#[extra-testing]
+#Include = /etc/pacman.d/mirrorlist
+
+[extra]
+Include = /etc/pacman.d/mirrorlist
+
+
+# If you want to run 32 bit applications on your x86_64 system,
+# enable the multilib repositories as required here.
+
+#[multilib-testing]
+#Include = /etc/pacman.d/mirrorlist
+
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+
+# An example of a custom package repository.  See the pacman manpage for
+# tips on creating your own repositories.
+#[custom]
+#SigLevel = Optional TrustAll
+#Server = file:///home/custompkgs
+" > /mnt/etc/pacman.conf
+
 # I don't know how to change the terminal without aborting the archinstall script.
 arch-chroot /mnt bash -c "
 
@@ -178,7 +272,9 @@ echo -e "KEYMAP=de-latin1" >  /etc/vconsole.conf;
 echo -e "archlinux" >  /etc/hostname;
 
 
-yes | pacman -Syyu networkmanager dolphin ark konsole sudo git grub nano efibootmgr plasma plasma-login-manager  acpid avahi iwd fish --disable-download-timeout --noconfirm;
+yes | pacman -Syyu networkmanager dolphin ark konsole sudo git grub nano efibootmgr plasma plasma-login-manager acpid avahi iwd fish --disable-download-timeout --noconfirm;
+
+yes | pacman -Syyu gamemod gamescope fastfetch --disable-download-timeout --noconfirm;
 
 chsh -s /bin/fish
 
@@ -198,6 +294,218 @@ grub-install --target=x86_64-efi --efi-directory=boot/ --bootloader-id=GRUB;
 
 grub-mkconfig -o /boot/grub/grub.cfg"
 
+echo -e "
+
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+
+" > /mnt/home/user/.config/fish/config.fish
+
+echo -e "
+
+# Don't edit this file, as it will be written by the web-config tool (`#fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+
+set --global fish_color_autosuggestion FFC473
+set --global fish_color_cancel --reverse
+set --global fish_color_command FF9400
+set --global fish_color_comment A63100
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end FF4C00
+set --global fish_color_error FFDD73
+set --global fish_color_escape 00a6b2
+set --global fish_color_gray
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_keyword
+set --global fish_color_normal normal
+set --global fish_color_operator 00a6b2
+set --global fish_color_option
+set --global fish_color_param FFC000
+set --global fish_color_quote BF9C30
+set --global fish_color_redirection BF5B30
+set --global fish_color_search_match white --bold --background=brblack
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline=single
+set --global fish_pager_color_background
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description B3A06D
+set --global fish_pager_color_prefix normal --bold --underline=single
+set --global fish_pager_color_progress brwhite --bold --background=cyan
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion
+set --global fish_pager_color_secondary_description
+set --global fish_pager_color_secondary_prefix
+set --global fish_pager_color_selected_background --background=brblack
+set --global fish_pager_color_selected_completion
+set --global fish_pager_color_selected_description
+set --global fish_pager_color_selected_prefix
+
+" > /mnt/home/user/.config/fish/conf.d/fish_frozen_theme.fish
+
+echo -e "
+
+# This file contains fish universal variable definitions.
+# VERSION: 3.0
+SETUVAR __fish_initialized:4300
+SETUVAR __fish_webconfig_theme_notification:set\x2dtheme\x2dv1\x2d\x231
+
+" > /mnt/home/user/.config/fish/fish_variables
+
+echo -e "
+
+function fish_prompt
+        # This prompt shows:
+        # - green lines if the last return command is OK, red otherwise
+        # - your user name, in red if root or yellow otherwise
+        # - your hostname, in cyan if ssh or blue otherwise
+        # - the current path (with prompt_pwd)
+        # - date +%X
+        # - the current virtual environment, if any
+        # - the current git status, if any, with fish_git_prompt
+        # - the current battery state, if any, and if your power cable is unplugged, and if you have "acpi"
+        # - current background jobs, if any
+
+        # It goes from:
+        # ┬─[nim@Hattori:~]─[11:39:00]
+        # ╰─>$ echo here
+
+        # To:
+        # ┬─[nim@Hattori:~/w/dashboard]─[11:37:14]─[V:django20]─[G:master↑1|●1✚1…1]─[B:85%, 05:41:42 remaining]
+        # │ 2    15054    0%    arrêtée    sleep 100000
+        # │ 1    15048    0%    arrêtée    sleep 100000
+        # ╰─>$ echo there
+
+        set -l retc red
+        test $status = 0; and set retc green
+
+        set -q __fish_git_prompt_showupstream
+        or set -g __fish_git_prompt_showupstream auto
+
+        function _nim_prompt_wrapper
+                set retc $argv[1]
+                set -l field_name $argv[2]
+                set -l field_value $argv[3]
+
+                set_color normal
+                set_color $retc
+                echo -n '─'
+                set_color -o green
+                echo -n '['
+                set_color normal
+                test -n $field_name
+                and echo -n $field_name:
+                set_color $retc
+                echo -n $field_value
+                set_color -o green
+                echo -n ']'
+        end
+
+        set_color $retc
+        echo -n '┬─'
+        set_color -o green
+        echo -n [
+
+        if functions -q fish_is_root_user; and fish_is_root_user
+                set_color -o red
+        else
+                set_color -o yellow
+        end
+
+        echo -n $USER
+        set_color -o white
+        echo -n @
+
+        if test -z "$SSH_CLIENT"
+                set_color -o blue
+        else
+                set_color -o cyan
+        end
+
+        echo -n (prompt_hostname)
+        set_color -o white
+        echo -n :(prompt_pwd)
+        set_color -o green
+        echo -n ']'
+
+        # Date
+        _nim_prompt_wrapper $retc '' (date +%X)
+
+        # Vi-mode
+
+        if test "$fish_key_bindings" = fish_vi_key_bindings
+                or test "$fish_key_bindings" = fish_hybrid_key_bindings
+                set -l mode
+                switch $fish_bind_mode
+                        case default
+                                set mode (set_color --bold red)N
+                        case operator f F t T
+                                set mode (set_color --bold cyan)N
+                        case insert
+                                set mode (set_color --bold green)I
+                        case replace_one
+                                set mode (set_color --bold green)R
+                        case replace
+                                set mode (set_color --bold cyan)R
+                        case visual
+                                set mode (set_color --bold magenta)V
+                end
+                set mode $mode(set_color normal)
+                _nim_prompt_wrapper $retc '' $mode
+        end
+
+        # Virtual Environment
+        set -q VIRTUAL_ENV_DISABLE_PROMPT
+        or set -g VIRTUAL_ENV_DISABLE_PROMPT true
+        set -q VIRTUAL_ENV
+        and _nim_prompt_wrapper $retc V (path basename "$VIRTUAL_ENV")
+
+        # git
+        set -l prompt_git (fish_git_prompt '%s')
+        test -n "$prompt_git"
+        and _nim_prompt_wrapper $retc G $prompt_git
+
+        # Battery status
+        type -q acpi
+        and acpi -a 2>/dev/null | string match -rq off
+        and _nim_prompt_wrapper $retc B (acpi -b | cut -d' ' -f 4-)
+
+        # New line
+        echo
+
+        # Background jobs
+        set_color normal
+
+        for job in (jobs)
+                set_color $retc
+                echo -n '│ '
+                set_color brown
+                echo $job
+        end
+
+        set_color normal
+        set_color $retc
+        echo -n '╰─>'
+        set_color -o red
+        echo -n '$ '
+        set_color normal
+end
+
+fastfetch
+
+" > /mnt/home/user/.config/fish/functions/fish_prompt.fish
+
 umount -R /mnt
 
 reboot now
+
